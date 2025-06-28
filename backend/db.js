8b30773a55109d2acddd0d2f6af78fe93c1f1e21
devang0426 <devang2626@gmail.com>
2025-06-28 18:17:38 +0530

    Initial commit with my code

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-// mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos
-// .env
-mongoose.connect("mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos")
+mongoose.connect("")//mongodb server
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
