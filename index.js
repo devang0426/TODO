21fd7927181c08ac10cd04756c2cc6606a28ac58
devang0426 <devang2626@gmail.com>
2025-06-28 17:13:07 +0530

    basic changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const { createTODO } = require("./types");
 const app=express();
 app.use(express.json());
 
@@ -6,7 +7,14 @@ app.use(express.json());
 
 
 app.post("/todo",function(req,res){
-
+    const createPayload=req.body;
+    const parsedPayload=createTODO.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"wrong inputs"
+        })
+        return;
+    }
 })
 
 
@@ -15,6 +23,15 @@ app.get("/todo",function(req,res){
 })
 
 
-app.post("/compleated",function(req,res){
-    
+app.put("/compleated",function(req,res){
+       
+const updatePayload=req.body;
+    const parsedPayload=createTODO.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"wrong inputs"
+        })
+        return;
+    }
+
 })
